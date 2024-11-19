8465b9738b186927391b646621fd876605cd4a39
AriLugassy <lugassa1@tcnj.edu>
2024-11-18 21:19:51 -0500

    more scroll troubles

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,13 @@
 window.addEventListener('scroll', () => {
+   if ((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight) {
+      document.getElementById("scroll-animation").style.backgroundImage = "url('assets/images/scroll-bottom-open.svg')"
+      console.log("youre at the bottom")
+    }
+    else {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+    }
   }, false);
+
+  window.onscroll = function(ev) {
+   
+};
