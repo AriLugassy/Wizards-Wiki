7438b9ab15eb02e43f4a516f2bb19136ecb57ad9
AriLugassy <lugassa1@tcnj.edu>
2024-11-24 16:33:09 -0500

    added profs help and notes

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -11,3 +11,22 @@ window.addEventListener('scroll', () => {
   window.onscroll = function(ev) {
    
 };
+
+
+let scrollAnimation = document.getElementById('scroll-animation')
+
+window.addEventListener('scroll', () => {
+  if((winder.innerHeight + Math.round(window.scrollY))>= document.body.offsetHeight){
+    scrollAnimation.style.backgroundImage = "url('assets/images/scroll-bottom-open.svg')";
+    scrollAnimation.style.animation = "none";
+    console.log("youre at the bottom")
+  }
+  else {
+    scrollAnimation.style.animation = "scrollmove 1s infinite";
+    scrollAnimation.style.animationDelay = " calc(var(--scroll) * -2s)"
+    scrollAnimation.style.animationPlayState = "paused"
+    document.body.style.setProperty('--scroll', window.scrollY / (document.body.offsetHeight - winow.innerHeight));
+
+    console.log("not at bottom")
+  }
+}, false);
