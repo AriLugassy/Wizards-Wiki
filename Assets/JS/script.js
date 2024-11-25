5b39bcb22a3e898413ffeedc3b3a4cec7438fc48
AriLugassy <lugassa1@tcnj.edu>
2024-11-25 14:07:17 -0500

    scroll almost finished

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,31 +1,27 @@
-window.addEventListener('scroll', () => {
-   if ((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight) {
-      document.getElementById("scroll-animation").style.backgroundImage = "url('assets/images/scroll-bottom-open.svg')"
-      console.log("youre at the bottom")
-    }
-    else {
-    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-    }
-  }, false);
-
-  window.onscroll = function(ev) {
-   
-};
-
 
 let scrollAnimation = document.getElementById("scroll-animation")
+let scrollAnimationTop = document.getElementById("scroll-animation-top")
 
 window.addEventListener('scroll', () => {
-  if((winder.innerHeight + Math.round(window.scrollY))>= document.body.offsetHeight) {
+  if((window.innerHeight + Math.round(window.scrollY))>= document.body.offsetHeight) {
     scrollAnimation.style.backgroundImage = "url('assets/images/scroll-bottom-open.svg')";
     scrollAnimation.style.animation = "none";
     console.log("youre at the bottom")
+  }
+  else if ((window.scrollY == 0)) {
+    scrollAnimationTop.style.backgroundImage = "url('assets/images/scroll-bottom-open.svg')";
+    scrollAnimationTop.style.animation = "none";
+    console.log("youre at the top")
+
   }
   else {
     scrollAnimation.style.animation = "scrollmove 1s infinite";
     scrollAnimation.style.animationDelay = " calc(var(--scroll) * -2s)"
     scrollAnimation.style.animationPlayState = "paused"
     document.body.style.setProperty('--scroll', window.scrollY / (document.body.offsetHeight - window.innerHeight));
+    scrollAnimationTop.style.animation = "scrollmove 1s infinite";
+    scrollAnimationTop.style.animationDelay = " calc(var(--scroll) * -2s)"
+    scrollAnimationTop.style.animationPlayState = "paused"
 
     console.log("not at bottom")
   }
